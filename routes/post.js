9f36e6fb94d587e3324082cc5a5b1908c134b8c8
SeongGwon2608 <insa8029@naver.com>
2021-03-09 01:09:41 +0900

    210309 01:10 유저 닉네임으로 게시글 검색 기능 추가

--- routes/post.js
+++ routes/post.js
@@ -54,4 +54,29 @@ router.post('/', login, upload2.none(), async (req, res, next) => {
     }
 });
 
+router.post('/search', login, async (req, res, next) => {
+    try {
+        //입력받은 유저 닉네임 저장
+        const searchid = req.body.searchID;
+
+        //유저 닉네임을 가지고 해당 멤버 검색
+        const searchmember = await Member.findOne({where: {nickname : searchid}});
+        //console.log(searchmember.email);
+
+        //210306 01:23 여기까지 일단 함
+        //검색을 다른 페이지로 빼서 해야할거 같음
+        //검색시 전송하는 데이터가 덮어씌워 버려서 member.id 값이 변경되어버림
+        const posts = await Post.findAll({where: {memberId: searchmember.id}});
+        res.render('search', {
+            title: '검색 결과',
+            members: searchmember,
+            twits: posts,
+        });
+        //res.redirect('/main');
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
+});
+
 module.exports = router;
